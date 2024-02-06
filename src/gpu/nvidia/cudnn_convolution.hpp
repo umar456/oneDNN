c66d6ddd48efc533ce743f827eb5a9f15ac83470
John Osorio <john.osorio@codeplay.com>
2024-02-06 15:52:11 +0000

    Enabling the use of postops and scaling

--- src/gpu/nvidia/cudnn_convolution.hpp
+++ src/gpu/nvidia/cudnn_convolution.hpp
@@ -41,8 +41,7 @@ struct cudnn_convolution_fwd_t : public primitive_t {
         pd_t(const pd_t &other)
             : cudnn_convolution_fwd_pd_t(other)
             , impl_(other.impl_)
-            , dst_md_temp_(other.dst_md_temp_)
-            , y_fp32_md_temp_(other.y_fp32_md_temp_) {}
+            , dst_md_temp_(other.dst_md_temp_) {}
 
         DECLARE_COMMON_PD_T("cuda:cudnn:any", cudnn_convolution_fwd_t);
 
@@ -98,17 +97,12 @@ struct cudnn_convolution_fwd_t : public primitive_t {
 
             if (check_for_zero_dims()) return status::success;
 
-            const bool use_temp_dst = attr()->post_ops_.len() > 0;
-            if (use_temp_dst) {
-                dst_md_temp_ = dst_md_;
-                if (dst_md_.data_type == s8) { dst_md_temp_.data_type = f32; }
-            }
-
             const bool use_scales_dst = !attr()->scales_.has_default_values()
                     && dst_md_.data_type == s8;
-            if (use_scales_dst) {
-                y_fp32_md_temp_ = dst_md_;
-                y_fp32_md_temp_.data_type = f32;
+            const bool use_temp_dst = attr()->post_ops_.len() > 0;
+            if (use_temp_dst || use_scales_dst) {
+                dst_md_temp_ = dst_md_;
+                if (dst_md_.data_type == s8) { dst_md_temp_.data_type = f32; }
             }
 
             impl_.reset(new cudnn_convolution_impl_fwd_t());
@@ -117,7 +111,6 @@ struct cudnn_convolution_fwd_t : public primitive_t {
         bool with_scratchpad() const { return impl_->with_scratchpad(); }
         std::shared_ptr<cudnn_convolution_impl_base_t> impl_;
         memory_desc_t dst_md_temp_;
-        memory_desc_t y_fp32_md_temp_;
 
         bool use_temp_dst() const {
             if (impl_.get()) return impl_->use_temp_dst();
@@ -174,36 +167,30 @@ struct cudnn_convolution_fwd_t : public primitive_t {
     };
 
     status_t init_temp_dst(engine_t *engine) {
+        const auto impl = pd()->impl_.get();
         auto sycl_engine = utils::downcast<sycl_cuda_engine_t *>(engine);
         memory_storage_t *scratch_ptr = nullptr;
         auto wrap = memory_desc_wrapper(pd()->dst_md_temp_);
-        CHECK(sycl_engine->create_memory_storage(
-                &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
-        scratch_storage.reset(scratch_ptr);
+        if (impl && impl->use_temp_dst()) {
+            CHECK(sycl_engine->create_memory_storage(
+                    &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
+            scratch_storage.reset(scratch_ptr);
 
-        CHECK(sycl_engine->create_memory_storage(
-                &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
-        scratch_storage_2.reset(scratch_ptr);
-
-        return status::success;
-    }
-
-    status_t init_temp_dst_scales(engine_t *engine) {
-        auto sycl_engine = utils::downcast<sycl_cuda_engine_t *>(engine);
-        memory_storage_t *scratch_ptr = nullptr;
-        auto wrap = memory_desc_wrapper(pd()->y_fp32_md_temp_);
-        CHECK(sycl_engine->create_memory_storage(
-                &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
-        scratch_storage_3.reset(scratch_ptr);
+            CHECK(sycl_engine->create_memory_storage(
+                    &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
+            scratch_storage_2.reset(scratch_ptr);
+        }
+        if (impl && impl->use_scales_dst()) {
+            CHECK(sycl_engine->create_memory_storage(
+                    &scratch_ptr, memory_flags_t::alloc, wrap.size(), nullptr));
+            scratch_storage_3.reset(scratch_ptr);
+        }
 
         return status::success;
     }
 
     virtual status_t init(engine_t *engine) override {
-        const auto impl = pd()->impl_.get();
-        if (impl && impl->use_temp_dst()) { init_temp_dst(engine); }
-
-        if (impl && impl->use_scales_dst()) { init_temp_dst_scales(engine); }
+        init_temp_dst(engine);
 
         return status::success;
     }
